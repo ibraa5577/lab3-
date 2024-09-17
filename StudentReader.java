ad873cf130d1fab249c2dc7a77d3c62f283a8c24
ibbd5577 <abrahymalshay5@gmail.com>
2024-09-17 13:35:02 +0300

    updating the reader method

---
+++ StudentReader.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class StudentReader {
+
+    public List<String> readStudentsFromFile(String filePath) {
+        List<String> students = null;
+        try {
+            students = Files.readAllLines(Paths.get(filePath));  
+        } catch (IOException e) {
+            e.printStackTrace(); 
+        }
+        return students;
+    }
+
+    public static void main(String[] args) {
+        StudentReader reader = new StudentReader();
+        List<String> students = reader.readStudentsFromFile("students.txt");
+
+     
+        if (students != null) {
+            for (String student : students) {
+                System.out.println(student);
+            }
+        }
+    }
+}
